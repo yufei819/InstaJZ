7b3a31c35b6bc20040e6fff71ccbc05a6207bfe7
yufei <yufei@umich.edu>
2020-04-26 16:33:48 -0700

    Power a static page

--- Insta/urls.py
+++ Insta/urls.py
@@ -16,7 +16,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+from Insta.views import HelloWorld
+
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('Insta/', include('Insta.urls'))
+    path('', HelloWorld.as_view(), name='HelloWorld')
 ]
